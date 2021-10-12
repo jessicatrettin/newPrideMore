c8c4ce6a841b5bdbad106da8de1a7b00654fb4d0
jessica.trettin@gmail.com <jessica.trettin@gmail.com>
2021-10-12 16:55:01 -0300

    Add project files.

---
+++ newPrideMore/Models/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace newPrideMore.Models.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
