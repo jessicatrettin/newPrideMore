cc9cc3522951f8f268dbde123861fb2846fce117
jessica.trettin@gmail.com <jessica.trettin@gmail.com>
2021-10-16 01:53:49 -0300

    CRUD Professional e CRUD ProfessionalType OK

--- newPrideMore/Controllers/ProfessionalsController.cs
+++ newPrideMore/Controllers/ProfessionalsController.cs
@@ -1,70 +1,159 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using newPrideMore.Models;
-using newPrideMore.Models.ViewModels;
-using newPrideMore.Services;
 
 namespace newPrideMore.Controllers
 {
     public class ProfessionalsController : Controller
     {
+        private readonly newPrideMoreContext _context;
 
-        private readonly ProfessionalService _professionalService;
-        private readonly ProfessionalTypeService _professionaTypeService;
-
-        public ProfessionalsController(ProfessionalService professionalService, ProfessionalTypeService professionalTypeService)
+        public ProfessionalsController(newPrideMoreContext context)
         {
-            _professionalService = professionalService;
-            _professionaTypeService = professionalTypeService;
+            _context = context;
         }
 
-        public IActionResult Index()
+        // GET: Professionals
+        public async Task<IActionResult> Index()
         {
-            var list = _professionalService.FindAll();
-            return View(list);
+            var newPrideMoreContext = _context.Professional.Include(p => p.ProfessionalType);
+            return View(await newPrideMoreContext.ToListAsync());
         }
 
-
-        public IActionResult FindProfessional()
+        // GET: Professionals/Details/5
+        public async Task<IActionResult> Details(string id)
         {
-            var list = _professionalService.FindAll();
-            return View(list);
-        }
-
-        public IActionResult Create()
-        {
-            var professionalTypes = _professionaTypeService.FindAll();
-            var viewModel = new ProfessionalFormViewModel { ProfessionalTypes = professionalTypes };
-            return View(viewModel);
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Create(Professional professional)
-        {
-            _professionalService.Insert(professional);
-            return RedirectToAction(nameof(Index));
-        }
-
-        public IActionResult Delete(string? id)
-        {
-            
             if (id == null)
             {
                 return NotFound();
             }
 
-            var obj = _professionalService.FindById(id.Value);
-            if (obj == null)
+            var professional = await _context.Professional
+                .Include(p => p.ProfessionalType)
+                .FirstOrDefaultAsync(m => m.Email == id);
+            if (professional == null)
             {
                 return NotFound();
             }
-            return View(obj);
 
-
+            return View(professional);
         }
-        /*public IActionResult Details(int? id)
-        {
 
-        }*/
+        // GET: Professionals/Create
+        public IActionResult Create()
+        {
+            ViewData["ProfessionalTypeId"] = new SelectList(_context.ProfessionalType, "Id", "Id");
+            return View();
+        }
+
+        // POST: Professionals/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Register,ProfessionalTypeId,Address,HealthInsurance,Email,Name,Cpf,Phone,Instagram,Password")] Professional professional)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(professional);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProfessionalTypeId"] = new SelectList(_context.ProfessionalType, "Id", "Id", professional.ProfessionalTypeId);
+            return View(professional);
+        }
+
+        // GET: Professionals/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var professional = await _context.Professional.FindAsync(id);
+            if (professional == null)
+            {
+                return NotFound();
+            }
+            ViewData["ProfessionalTypeId"] = new SelectList(_context.ProfessionalType, "Id", "Id", professional.ProfessionalTypeId);
+            return View(professional);
+        }
+
+        // POST: Professionals/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Register,ProfessionalTypeId,Address,HealthInsurance,Email,Name,Cpf,Phone,Instagram,Password")] Professional professional)
+        {
+            if (id != professional.Email)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(professional);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProfessionalExists(professional.Email))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProfessionalTypeId"] = new SelectList(_context.ProfessionalType, "Id", "Id", professional.ProfessionalTypeId);
+            return View(professional);
+        }
+
+        // GET: Professionals/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var professional = await _context.Professional
+                .Include(p => p.ProfessionalType)
+                .FirstOrDefaultAsync(m => m.Email == id);
+            if (professional == null)
+            {
+                return NotFound();
+            }
+
+            return View(professional);
+        }
+
+        // POST: Professionals/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var professional = await _context.Professional.FindAsync(id);
+            _context.Professional.Remove(professional);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProfessionalExists(string id)
+        {
+            return _context.Professional.Any(e => e.Email == id);
+        }
     }
 }
